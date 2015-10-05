1a07e723d382b949b51b7e56b51d51556e399bdd
Janik.Brischle <Janik.Brischle@SK-002268594A48>
2015-10-05 15:33:01 +0200

    Konstruktoren und Methoden zu GeometricObject hinzugefügt

--- animationGame/src/animationGame/GeometricObject.java
+++ animationGame/src/animationGame/GeometricObject.java
@@ -1,6 +1,7 @@
 package animationGame;
 
-public class GeometricObject {
+public class GeometricObject 
+{
 
 	public Vertex pos;
 	public double hate;
@@ -10,20 +11,73 @@ public class GeometricObject {
 	public double getLove(){return love; }
 	public Vertex getPos(){return pos;}
 
-public GeometricObject(double hate, double love, Vertex pos){
+public GeometricObject(double hate, double love, Vertex pos)
+ {
 	this.pos=pos;
 	this.love=love;
 	this.hate=hate;
 	
-	if(love<0){
+	if(love<0)
+	{
 		love = -love;
 		pos.x = pos.x -love;
 	}
-	if(hate<0){
+	if(hate<0)
+	{
 		hate = -hate;
 		pos.y = pos.y -hate;
 	}
  }
+public GeometricObject(double hate, double love)
+ {
+	this(hate, love, new Vertex(0,0));
+ }
 
+public GeometricObject(double hate)
+ {
+	this(hate,hate);
+ }
+ 
+public GeometricObject()
+ {
+	this(10);
+ }
+
+public GeometricObject(Vertex pos)
+ {
+	this(0,0,pos);
+ }
+
+public String toString()
+ {
+	return "width="+love+"height="+hate+"position="+pos;
+	
+ }
+
+public double cirumference()
+ {
+	return 2*(love+hate);
+ }
+
+public double area()
+ {
+	return love*hate;
+ }
+public boolean contains(Vertex v)
+ {
+	return v.x >= pos.x && v.x <= pos.x+love && 
+		   v.y >= pos.y && v.y <= pos.y+hate;
+ }
+
+public boolean isLargerThan(GeometricObject that)
+ {
+	return that.area() > this.area();
+ }
+
+public void moveTo(Vertex pos)
+ {
+	this.pos=pos;
+ }
 }
+
 
