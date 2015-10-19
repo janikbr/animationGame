98c34b6692158b830d3f98817a5fd62cb94b03ff
Janik.Brischle <Janik.Brischle@SK-D8CB8A509AF5>
2015-10-19 15:32:46 +0200

    ...

---
+++ animationGame/src/animationGame/SimpleTriangle.java
@@ -0,0 +1,18 @@
+package animationGame;
+public class SimpleTriangle extends GeometricObject 
+{
+
+	public SimpleTriangle (double w, double h, Vertex pos)
+	{
+		super(w,h,pos);
+	} 
+
+	@Override public double area()
+		
+	{
+		return Math.
+	}
+
+
+
+}
